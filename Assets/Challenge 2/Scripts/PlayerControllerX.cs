6205a8904b7d736ceb2d0446521a1830d7afac67
luksim01 <145286746+luksim01@users.noreply.github.com>
2023-09-30 16:32:37 +0100

    dog spawn cooldown added

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool dogCooldown = false;
+    private float cooldownTime = 3;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog provided dog isn't in cooldown
+        if (Input.GetKeyDown(KeyCode.Space) && !dogCooldown)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // coroutine runs spawnDog method asynchronously with the main game loop
+            StartCoroutine("spawnDog");
         }
+    }
+
+    IEnumerator spawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        dogCooldown = true;
+        // yield pauses spawnDog method until cooldown timer is done
+        yield return new WaitForSeconds(cooldownTime);
+        dogCooldown = false;
     }
 }
